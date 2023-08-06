f815bbbc01303690bd0d436ed69d1df705de1346
ginlink <ginlink1998@gmail.com>
2023-08-06 22:45:06 +0800

    chore: rename

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     // },
 
     socialLinks: [
-      { icon: "github", link: "https://github.com/ginlink/gin-navs" },
+      { icon: "github", link: "https://github.com/ginlink/site-nav" },
     ],
   },
 });
